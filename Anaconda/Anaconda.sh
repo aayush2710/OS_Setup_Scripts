d89350689f13ac3952a480243c88d5f0f54027af
Aayush Goyal <aayush.goyal54@gmail.com>
2020-12-07 01:49:54 +0530

    update

--- Anaconda/Anaconda.sh
+++ Anaconda/Anaconda.sh
@@ -17,7 +17,8 @@ source ~/.zshrc
 #Create Launcher Icons
 sudo cp Anaconda.desktop /usr/share/applications
 sudo cp Spyder.desktop /usr/share/applications
-sudo echo " Icon=/home/$USER/anaconda3/lib/python3.8/site-packages/anaconda_navigator/static/images/logos/spyder.png;" >> /usr/share/applications/Spyder.desktop
+echo " Icon=/home/$USER/anaconda3/lib/python3.8/site-packages/anaconda_navigator/static/images/anaconda-icon-256x256.png" | sudo tee -a /usr/share/applications/Anaconda.desktop
+echo " Icon=/home/$USER/anaconda3/lib/python3.8/site-packages/anaconda_navigator/static/images/logos/spyder.png" | sudo tee -a /usr/share/applications/Spyder.desktop
 
 
 #Conda Packages
