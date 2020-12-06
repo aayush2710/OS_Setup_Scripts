99813e42594247203aec9627e729bb7d1e1a2b63
Aayush Goyal <aayush.goyal54@gmail.com>
2020-12-06 14:28:19 +0530

    update

--- Anaconda/Anaconda.sh
+++ Anaconda/Anaconda.sh
@@ -1,6 +1,7 @@
 #Install Anaconda
 pkgver=2020.11
 pythonver=3.8
+
 wget https://repo.anaconda.com/archive/Anaconda3-$pkgver-Linux-x86_64.sh
 bash Anaconda3-$pkgver-Linux-x86_64.sh -b -p $HOME/anaconda3
 sudo rm -rf /home/$USER/anaconda3/lib/python$pythonver/site-packages/anaconda_navigator/api/external_apps/vscode.py
@@ -8,9 +9,9 @@ source /home/$USER/anaconda3/bin/activate
 conda init
 conda init zsh
 source ~/.zshrc
-sudo cp Anaconda.sh /usr/share/applications
-sudo cp Spyder.sh /usr/share/applications
-
+sudo cp Anaconda.desktop /usr/share/applications
+sudo cp Spyder.desktop /usr/share/applications
+sudo echo " Icon=/home/$USER/anaconda3/lib/python3.8/site-packages/anaconda_navigator/static/images/logos/spyder.png;" >> /usr/share/applications/Spyder.desktop
 #Conda Packages
 
 conda install -y pytorch torchvision cpuonly -c pytorch
